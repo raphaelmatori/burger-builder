e0096dea56acc56b61e0cd0761316550190cd8ed
Raphael Matori <raphaelmatori@live.com>
2020-08-18 18:55:51 -0300

    Handling errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,8 +5,9 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import classes from './BurgerBuilder.module.scss';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
-import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     tomatoes: 0.3,
@@ -162,4 +163,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
